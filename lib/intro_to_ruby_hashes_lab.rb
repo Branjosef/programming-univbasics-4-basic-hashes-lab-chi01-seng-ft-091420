b566314c538a2de7f76839888bbe13656d00e823
Brandon Fonnemann <branjosef@gmail.com>
2020-08-20 00:35:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,16 +5,17 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  my_hash = {name: "Bob", email: "Bob@example.com"}
+  my_hash = {name: "Bob"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  
+  pioneer{name: 'Grace Hopper'}
 
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  id_hash_generator = {id: (number)}
 end
